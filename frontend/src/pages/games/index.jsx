c2c3bb23d6e1ba3c9065096f561051a6fb00d05b
ayan <ayanxa-af206@code.edu.az>
2025-02-08 20:57:06 +0400

    .

---
+++ frontend/src/pages/games/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Games() {
+  return (
+    <div className='h-screen'>
+      
+    </div>
+  )
+}
+
+export default Games
---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div style={{height:"100vh"}}>
+      
+    </div>
+  )
+}
+
+export default Home
